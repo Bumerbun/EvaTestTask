8c6c3feff80044f3d3bfa8ac5e533989860e56de
Bumerbun <Alex03xxxx@gmail.com>
2023-09-16 22:39:21 +0300

    messangers limitations data fill

--- app.vue
+++ app.vue
@@ -10,11 +10,94 @@ export default {
   data(){
     return {
       optionsData: [
-        {'label': "ВК", 'ref': "vk"},
-        {'label': "WhatsApp", 'ref': "whatsapp"},
-        {'label': "Telegram", 'ref': "tg"},
-        {'label': "SMS", 'ref': "sms"},
-      ]
+        {'label': "ВК", 
+        'ref': "vk",
+        'limitations': {
+          'maxTextSize' : 4069,
+          'customKeyboardSupport': true,
+          'standardKeyboardSupport': true,
+          'standardKeyboardLimitations': {
+            'maxButtonCount': 40,
+            'maxButtonTextSize' : -1,
+            'linkButtonSupport': true,
+            'maxLinkButtonCount': -1
+          },
+          'inlineKeyboardSupport': true,
+          'inlineKeyboardLimitations':  {
+            'maxButtonCount': 10,
+            'maxButtonTextSize' : -1,
+            'linkButtonSupport': true,
+            'maxLinkButtonCount': -1
+          },
+        }
+      },
+        {
+          'label': "WhatsApp", 
+          'ref': "whatsapp",
+          'limitations': {
+            'maxTextSize' : 1000,
+            'customKeyboardSupport': true,
+            'standardKeyboardSupport': true,
+            'standardKeyboardLimitations': {
+              'maxButtonCount': 10,
+              'maxButtonTextSize' : 20,
+              'linkButtonSupport': false,
+              'maxLinkButtonCount': 0
+            },
+            'inlineKeyboardSupport': true,
+            'inlineKeyboardLimitations':  {
+              'maxButtonCount': 3,
+              'maxButtonTextSize' : 20,
+              'linkButtonSupport': true,
+              'maxLinkButtonCount': 1  
+          },
+        }
+      },
+        {
+          'label': "Telegram", 
+          'ref': "tg",
+          'limitations': {
+            'maxTextSize' : 4096,
+            'customKeyboardSupport': true,
+            'standardKeyboardSupport': true,
+            'standardKeyboardLimitations': {
+              'maxButtonCount': -1,
+              'maxButtonTextSize' : -1,
+              'linkButtonSupport': false,
+              'maxLinkButtonCount': 0
+
+            },
+            'inlineKeyboardSupport': true,
+            'inlineKeyboardLimitations':  {
+              'maxButtonCount': -1,
+              'maxButtonTextSize' : 64,
+              'linkButtonSupport': true,
+              'maxLinkButtonCount': -1
+          },
+        }
+        },
+        {
+          'label': "SMS", 
+          'ref': "sms",
+          'limitations': {
+            'maxTextSize' : 1000,
+            'customKeyboardSupport': true,
+            'standardKeyboardSupport': true,
+            'standardKeyboardLimitations': {
+              'maxButtonCount': 10,
+              'maxButtonTextSize' : 20,
+              'linkButtonSupport': false
+            },
+            'inlineKeyboardSupport': true,
+            'inlineKeyboardLimitations':  {
+              'maxButtonCount': 3,
+              'maxButtonTextSize' : 20,
+              'linkButtonSupport': true,
+              'maxLinkButtonCount': 1  
+          },
+        }
+        },
+      ],
     }
   }
 }
